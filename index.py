b6c32dfde7bd8cd0dda6e06398706df62d65f4b4
dudatt <dudamelobarbosa@gmail.com>
2024-04-20 01:57:10 -0300

    criando funções e atualizando

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 import bcrypt; #pip install bcrypt
+import json;
 
 nameUser = input("Digite seu nome de usuário: ");
 senha = input("Digite sua senha: ");
+
+def criarUserJson(nameUser, senha):
+    return { "UserName": nameUser, "Password": senha };
 
 def criptografiaSenha(senha):
     salt = bcrypt.gensalt(8)  # Gera um salt (complexidade adicional) para a criptografia. 
@@ -11,10 +15,21 @@ def criptografiaSenha(senha):
     return hashSenha
     # Cria o hash da senha combinando a senha digitada pelo usuário com o "salt" gerado.
     #  O "encode()"" é usado para converter a senha de uma string para uma sequência de bytes, que é o formato necessário para a função "bcrypt.hashpw()".
+def salvarUserJson(users, arquivo):
+    with open(arquivo, 'w') as f:
+        json.dump(users, f);
 
     # print(f"Senha original: {senha}")
     # print(f"Salt gerado: {salt.decode()}")
     # print(f"Hash da senha: {hashSenha.decode()}");
+def adicionarUserJson(users, nameUser, senha):
+    usuarioNovo = criarUserJson(nameUser, senha);
+    users.append(usuarioNovo);
+
+def criptografiaSenha(senha):
+    salt = bcrypt.gensalt(8);
+    hashSenha = bcrypt.hashpw(senha.encode(), salt);
+    return hashSenha;
 
 def autenticacao(user, senha, hashSenha):
     print("------------------");
@@ -26,25 +41,36 @@ def autenticacao(user, senha, hashSenha):
 
     if loginUser != user:
         print("Usuário ou senha incorretos.");
-    elif senhaUser != senha:
-        for i in range(5):
-            i += 1;
 
-            if bcrypt.checkpw(senhaUser.encode(), hashSenha):
-                print(f"Seja bem-vindo(a) {user}!");
-                break;
-            else:
-                print("Senha incorreta. Tente novamente.");
-                senhaUser = input("Insira sua senha: ");
-                bcrypt.checkpw(senhaUser.encode(), hashSenha);
-                print(f"Total de tentativas ({i}/5)");
-                if i == 5:
-                    print("Tentativas máximas alcançadas. Tente novamente mais tarde.");
-                continue;
-
-    elif bcrypt.checkpw(senhaUser.encode(), hashSenha) and loginUser == user:
-        print(f"Seja bem-vindo(a) {user}!");
-    else:
-        print("Tente novamente mais tarde.")
+def autenticacao(user, senha, hashSenha):
+    print("Tente novamente mais tarde.")
 
 print(autenticacao(nameUser, senha, criptografiaSenha(senha)));
+
+print("----------------------");
+print("Escolha uma opção: ")
+print("1 - Cadastrar");
+print("2 - Autenticar");
+resp = input("3 - Sair  ");
+
+if resp == "1" :
+    print("----------------------");
+    nameUser = input("Digite seu nome de usuário: ");
+    senha = input("Digite sua senha: ");
+    users = [];
+    adicionarUserJson(users, nameUser, senha);
+    print("1° parte feita");
+
+elif resp == "2":
+    print("----------------------");
+    nameUser = input("Digite seu nome de usuário: ");
+    senha = input("Digite sua senha: ");
+    autenticacao(nameUser, senha, criptografiaSenha(senha))
+
+else:
+    print("Insira uma resposta válida.")
+
+arquivoUsers = "usuarios.json";
+salvarUserJson(users, arquivoUsers);
+
+#print(autenticacao(nameUser, senha, criptografiaSenha(senha)));
