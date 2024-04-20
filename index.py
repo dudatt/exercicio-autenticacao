1053bbf51ef6871a4e41b647d7b0112e80990af3
dudatt <dudamelobarbosa@gmail.com>
2024-04-20 04:15:59 -0300

    up com  lista de usuários

--- index.py
+++ index.py
@@ -2,41 +2,49 @@ import json
 import bcrypt
 import getpass
 import sys
-
-global username 
-global senha 
-global loginUser
-global loginSenha
+import os
 
 def criarUsuarioJson(username, senha): # cria o objeto usuário
     return { "Username": username, "Password": senha }
 
-def adicionarUserJson(users, username, senha): # adiciona o objeto usuário na lista 
-    usuarioNovo = criarUsuarioJson(username, senha);
+def adicionarUserJson(users, username, senha): # adiciona o usuário na lista 
+    usuarioNovo = {"nome": username, "senha": senha};
     users.append(usuarioNovo);
+    return users
 
 def salvarUserJson(users, arquivo): # salva a lista de usuários no arquivo json
     with open(arquivo, 'w') as f:
-        json.dump(users, f);
+        json.dump(users, f, indent=4);
 
 def cadastrarUsuario():
-    username = input("Digite seu nome de usuário: ")
-    senha = getpass.getpass("Digite sua nova senha: ")
+    while True:
+        username = input("Digite seu nome de usuário: ")
+        senha = getpass.getpass("Digite sua nova senha: ")
 
-    salt = bcrypt.gensalt();
-    hashSenha = bcrypt.hashpw(senha.encode(), salt);
+        salt = bcrypt.gensalt();
+        hashSenha = bcrypt.hashpw(senha.encode(), salt);
+        hashSenha = hashSenha.decode()
 
-    hashSenha = hashSenha.decode()
+        arquivoUsers = "usuarios.json"
 
-    users = []
-    arquivoUsers = "usuarios.json"
+        try:
+            with open(arquivoUsers, 'r') as file:
+                users = json.load(file)
+        except FileNotFoundError:
+            users = []
 
-    criarUsuarioJson(username, hashSenha)
-    adicionarUserJson(users, username, hashSenha)
-    salvarUserJson(users, arquivoUsers)
-    print("Usuário cadastrado com sucesso!")
-    print("----------------------")
-    return
+        #criarUsuarioJson(username, hashSenha)
+        users = adicionarUserJson(users, username, hashSenha)
+        salvarUserJson(users, arquivoUsers)
+        print("Usuário cadastrado com sucesso!")
+        print("----------------------")
+        print("Deseja cadastrar outro usuário? (S/N)")
+        resp = input()
+        print("----------------------")
+        if resp == "N":
+            return False
+        elif resp == "S":
+            continue
 
 def autenticarUsuario():
     loginUser = input("Digite seu nome de usuário: ")
@@ -66,8 +74,30 @@ def autenticarUsuario():
                             print("Tentativas máximas alcançadas. Tente novamente mais tarde.");
                             sys.exit();
         else:
-            print("Usuário não encontrado.")
+            print("Usuário ou senha inválidos.")
             sys.exit();
+
+def verificarPermissaoArquivo(arquivo): #testar isso aki depois
+    
+    arquivoUsersDados = "usuarios.json"
+    with open(arquivoUsersDados, 'r') as f:
+        users = json.load(f)
+
+    for user in users:
+        if user["Username"].access(arquivo, user["Username"].R_OK):
+            print("Você tem permissão de leitura para o arquivo.")
+        else:
+            print("Você não tem permissão de leitura para o arquivo.")
+
+        if user["Username"].access(arquivo, user["Username"].W_OK):
+            print("Você tem permissão de escrita para o arquivo.")
+        else:
+            print("Você não tem permissão de escrita para o arquivo.")
+
+        if user["Username"].access(arquivo, user["Username"].X_OK):
+            print("Você tem permissão de execução para o arquivo.")
+        else:
+            print("Você não tem permissão de execução para o arquivo.")
 
 def criarArquivo():
         nomeArquivo = input("Digite o nome do arquivo: ")
@@ -88,14 +118,27 @@ print("----------------------")
 
 if resp == "1":
     cadastrarUsuario()
-    
 elif resp == "2":
     autenticarUsuario()
     print("----------------------")
     print("Comandos disponíveis: ")
     print("1 - Criar arquivo")
-    print("2 - Sair")
+    print("2 - Ler arquivo")
+    print("3 - Excluir arquivo")
+    print("4 - Executar arquivo") #extra
+    print("5 - Sair")
     print("----------------------")
     resp = input()
     print("----------------------")
+    if resp == "1":
+        criarArquivo()
+    elif resp == "2":
+        print("Ler arquivo")
+    elif resp == "3":
+        print("Excluir arquivo")
+    elif resp == "4":
+        print("Executar arquivo")
+    elif resp == "5":
+        print("Até mais!")
+        sys.exit()
     
