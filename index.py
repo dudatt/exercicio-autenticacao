dd51a1fde938339f959b858caae5da49742faee9
dudatt <dudamelobarbosa@gmail.com>
2024-04-20 14:40:05 -0300

    começando parte de permissões do usuário

--- index.py
+++ index.py
@@ -4,17 +4,44 @@ import getpass
 import sys
 import os
 
-def criarUsuarioJson(username, senha): # cria o objeto usuário
-    return { "Username": username, "Password": senha }
+# def criarUsuarioJson(username, senha): # cria o objeto usuário
+#    return { "Username": username, "Password": senha }
 
 def adicionarUserJson(users, username, senha): # adiciona o usuário na lista 
-    usuarioNovo = {"nome": username, "senha": senha};
+    usuarioNovo = {"Username": username, "Password": senha};
     users.append(usuarioNovo);
     return users
 
 def salvarUserJson(users, arquivo): # salva a lista de usuários no arquivo json
     with open(arquivo, 'w') as f:
         json.dump(users, f, indent=4);
+
+def criarPermissiosJson():
+    arquivoUsersDados = "usuarios.json"
+    with open(arquivoUsersDados, 'r') as f:
+        users = json.load(f)
+
+    arquivoPermissao = "permissoes.json"
+
+
+    for i in range(3): #ver isso aq
+        permissao = []
+        resp1 = input("Digite a permissão que deseja conceder (r, w, x): ").lower()
+        permissao.append(resp1)
+        for j in range(2):
+           resp2 = input("Mais alguma permissão? (S/N) ").upper()
+            if resp2 == "N":
+                break
+            elif resp2 == "S":
+                continue
+            else:
+                print("Opção inválida.")
+                sys.exit()
+
+    for user in users:
+        userPermissao = {"Username": user["Username"], "Permissions": permissao}
+
+    salvarUserJson(userPermissao, arquivoPermissao)
 
 def cadastrarUsuario():
     while True:
@@ -36,10 +63,12 @@ def cadastrarUsuario():
         #criarUsuarioJson(username, hashSenha)
         users = adicionarUserJson(users, username, hashSenha)
         salvarUserJson(users, arquivoUsers)
+        criarPermissiosJson()
+        #darPermissao()
         print("Usuário cadastrado com sucesso!")
         print("----------------------")
         print("Deseja cadastrar outro usuário? (S/N)")
-        resp = input()
+        resp = input().upper()
         print("----------------------")
         if resp == "N":
             return False
@@ -77,7 +106,7 @@ def autenticarUsuario():
             print("Usuário ou senha inválidos.")
             sys.exit();
 
-def verificarPermissaoArquivo(arquivo): #testar isso aki depois
+def verificarPermissaoArquivo(arquivo): #rever isso dps
     
     arquivoUsersDados = "usuarios.json"
     with open(arquivoUsersDados, 'r') as f:
@@ -141,4 +170,3 @@ elif resp == "2":
     elif resp == "5":
         print("Até mais!")
         sys.exit()
-    
